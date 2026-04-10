a83281eb95cf9b78ddfea291424ee22c5b7d4919
Felipe Salvador <dev@NSN1191261218.edu119.local>
2026-04-10 13:14:42 -0300

    Realizando os botões para trocar a cor

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,41 @@
-import styles from "./style.module.css"
-import {House} from 'lucide-react'
-import {History} from "lucide-react"
-import {Settings} from "lucide-react"
-import {Sun} from "lucide-react"
+import styles from './style.module.css';
+import { Home, History, Settings, Sun, Moon } from 'lucide-react';
+import { useState } from 'react';
 
-export function Menu(){
-    return(
-        <>
-        <div className= {styles.menu}>
-            <a className = {styles.menuLink} href="#"><House/>
-            </a>
-            <a className = {styles.menuLink} href="#"><History/>
-            </a>
-            <a className = {styles.menuLink} href="#"><Settings/>
-            </a>
-            <a className = {styles.menuLink} href="#"><Sun/>
-            </a>                    
-        </div>
-        </>
-    )
+type AvailableThemes = 'dark' | 'light';
+
+export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  }
+
+  return (
+    <nav className={styles.menu}>
+      <a className={styles.menuLink} href="#" aria-label="Home">
+        <Home />
+      </a>
+
+      <a className={styles.menuLink} href="#" aria-label="History">
+        <History />
+      </a>
+
+      <a className={styles.menuLink} href="#" aria-label="Settings">
+        <Settings />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href="#"
+        onClick={handleThemeChange}
+        aria-label="Mudar Tema"
+      >
+        {theme === 'dark' ? <Sun /> : <Moon />}
+      </a>
+    </nav>
+  );
 }
