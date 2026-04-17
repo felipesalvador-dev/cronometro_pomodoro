4cf357b76a7c161edddf47e27460e55ab3bfcfd8
Felipe Salvador <dev@NSN1191261218.edu119.local>
2026-04-17 08:50:27 -0300

    Adicionando light theme ao projeto

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,40 +1,80 @@
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './style.module.css';
-import { Home, History, Settings, Sun, Moon } from 'lucide-react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
     event.preventDefault();
-    setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
   }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#" aria-label="Home">
-        <Home />
-      </a>
-
-      <a className={styles.menuLink} href="#" aria-label="History">
-        <History />
-      </a>
-
-      <a className={styles.menuLink} href="#" aria-label="Settings">
-        <Settings />
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
+        <HouseIcon />
       </a>
 
       <a
         className={styles.menuLink}
-        href="#"
-        onClick={handleThemeChange}
-        aria-label="Mudar Tema"
+        href='#'
+        aria-label='Ver Histórico'
+        title='Ver Histórico'
       >
-        {theme === 'dark' ? <Sun /> : <Moon />}
+        <HistoryIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
+        <SettingsIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar Tema'
+        title='Mudar Tema'
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
